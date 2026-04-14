a2f02b3d572b784e2787dee73a62f173834188a4
sachinyd0021-cyber <sachinyd.0021@gmail.com>
2026-04-14 20:43:47 +0530

    Update server port to 5000 as fallback

--- backend/index.js
+++ backend/index.js
@@ -73,7 +73,7 @@ app.get("/", (req, res) => {
 });
 
 // 6. Port
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
